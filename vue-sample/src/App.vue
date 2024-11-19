b986580925f34e51f20f3434a6f266bfbe4187f7
yanshou517 <ra89517r@gmail.com>
2024-11-19 12:06:49 +0800

    [更]漢堡選單暫存

--- vue-sample/src/App.vue
+++ vue-sample/src/App.vue
@@ -30,12 +30,13 @@ const activeIndex = ref(null);
 const setActive = (index) => {
   activeIndex.value = index;
 };
+const active = ref(true);
 </script>
 
 <template>
   <div class="common-layout">
     <el-container>
-      <el-aside class="aside">
+      <el-aside class="aside" v-show="false">
         <div class="aside-icon">
           <div class="head"></div>
           <div class="beak"></div>
@@ -56,7 +57,16 @@ const setActive = (index) => {
         </div>
       </el-aside>
       <el-container>
-        <el-header class="header" v-show="false"></el-header>
+        <el-header class="header" v-show="true">
+          <div class="header-menu">X</div>
+          <div class="header-title">白頭翁不吃小米</div>
+          <div class="header-icon">
+            <div class="head"></div>
+            <div class="beak"></div>
+            <div class="face"></div>
+            <div class="eye"></div>
+          </div>
+        </el-header>
         <el-main class="main">
           <div class="main-title">白頭翁 (Chinese bulbul)</div>
           <div class="main-content">
@@ -111,10 +121,16 @@ const setActive = (index) => {
     align-items: center;
     z-index: 9999;
 
-    .head {
+    .head,
+    .face,
+    .beak,
+    .eye {
       position: absolute;
-      border-radius: 51% 33% 36% 20% / 49% 32% 45% 0%;
       background-color: black;
+    }
+
+    .head {
+      border-radius: 51% 33% 36% 20% / 49% 32% 45% 0%;
       width: 60%;
       height: 50%;
       z-index: 1;
@@ -129,8 +145,6 @@ const setActive = (index) => {
     }
 
     .eye {
-      position: absolute;
-      background-color: black;
       width: 10px;
       height: 10px;
       border-radius: 50%;
@@ -141,8 +155,6 @@ const setActive = (index) => {
     }
 
     .beak {
-      position: absolute;
-      background-color: black;
       width: 30px;
       height: 10px;
       border-radius: 40% 60% 28% 20% / 0% 100% 0% 0%;
@@ -185,11 +197,6 @@ const setActive = (index) => {
     text-underline-offset: 4.21px;
   }
 }
-
-/* .container-main {
-  width: 1095px;
-  height: 1001.36px;
-} */
 
 .main {
   width: 1095px;
@@ -296,6 +303,203 @@ const setActive = (index) => {
     position: absolute;
     bottom: 49px;
     left: 78.84px;
+  }
+}
+
+@media (max-width: 376px) {
+  .header {
+    height: 87.74px;
+    width: 100%;
+    position: relative;
+
+    .headerActive {
+      height: 286.43px;
+      border: 2px solid black;
+    }
+
+    .header-menu {
+      height: 18px;
+      width: 24px;
+      text-align: center;
+      border: 2px solid black;
+      position: absolute;
+      inset: 34.87px 324.62px 34.87px 26.38px;
+    }
+
+    .header-title {
+      position: absolute;
+      inset: 30.37px 117.5px;
+      width: 140px;
+      height: 27px;
+      font-size: 20px;
+      font-weight: 700;
+      line-height: 27.24px;
+    }
+    .header-icon {
+      position: absolute;
+      height: 48.78px;
+      width: 48.52px;
+      inset: 19.61px 18.84px 19.61px 307.38px;
+      background-color: white;
+      border-radius: 50%;
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 9999;
+
+      .head,
+      .face,
+      .beak,
+      .eye {
+        position: absolute;
+        background-color: black;
+      }
+
+      .head {
+        border-radius: 51% 33% 36% 20% / 49% 32% 45% 0%;
+        width: 60%;
+        height: 50%;
+        z-index: 1;
+      }
+
+      .face {
+        border-radius: 40% 32% 28% 20% / 65% 42% 34% 0%;
+        background-color: white;
+        width: 60%;
+        height: 30%;
+        z-index: 3;
+      }
+
+      .eye {
+        width: 6px;
+        height: 6px;
+        border-radius: 50%;
+        top: 45%;
+        left: 60%;
+        transform: translateX(-50%);
+        z-index: 4;
+      }
+
+      .beak {
+        width: 15px;
+        height: 5px;
+        border-radius: 40% 60% 28% 20% / 0% 100% 0% 0%;
+        top: 40%;
+        left: 80%;
+        transform: translateX(-50%) rotate(-10deg);
+        z-index: 2;
+      }
+    }
+  }
+  .main {
+    width: 1095px;
+    height: 634.21px;
+    position: relative;
+    overflow: hidden;
+
+    .main-title {
+      font-size: 48px;
+      font-weight: 700;
+      line-height: 65.38px;
+      color: white;
+      position: absolute;
+      top: 503.36px;
+      right: 44.75px;
+      bottom: 65.85px;
+      left: 506.25px;
+    }
+
+    .main-content {
+      font-size: 18px;
+      font-weight: 400;
+      line-height: 24.52px;
+      color: white;
+      position: absolute;
+      top: 572.36px;
+      right: 44.75px;
+      bottom: 36.85px;
+      left: 405.25px;
+    }
+
+    .img-mask {
+      width: auto;
+      height: 634.21px;
+    }
+
+    .img-mask img {
+      position: absolute;
+      width: 2034px;
+      height: 1110.88px;
+      top: -234.06px;
+      right: -312.4px;
+      bottom: -246.61px;
+      left: -626.6px;
+      z-index: -1;
+    }
+  }
+
+  .footer {
+    background-color: #dcccbc;
+    width: 1095px;
+    height: 367.15px;
+    display: flex;
+    overflow: hidden;
+
+    .footer-div {
+      width: 278.51px;
+      height: 225px;
+      margin: 58px 0px 85px 58px;
+      position: relative;
+    }
+
+    .footer-div:nth-child(1) {
+      margin-left: 50.74px;
+    }
+
+    .footer-div:last-child {
+      margin-right: 50.74px;
+    }
+
+    .footer-title {
+      width: 36px;
+      height: 80px;
+      font-size: 36px;
+      font-weight: 700;
+      line-height: 40px;
+      position: absolute;
+      right: 37.84px;
+      bottom: 145px;
+      left: 5px;
+    }
+
+    .title-icon {
+      width: 25px;
+      height: 25px;
+      background-color: transparent;
+      border: 8px solid rgba(170, 102, 102, 0.6);
+      border-radius: 50%;
+      box-sizing: border-box;
+      background: transparent;
+      position: absolute;
+      top: 66.58px;
+      right: 221.57px;
+      bottom: 113.42px;
+      left: 31.94px;
+    }
+
+    .footer-content {
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 21.79px;
+      width: 199.66px;
+      height: 176px;
+      position: absolute;
+      bottom: 49px;
+      left: 78.84px;
+    }
   }
 }
 </style>
